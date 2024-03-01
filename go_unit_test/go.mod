419e39a0183ed549913872616c63a3605d301b36
Nathaniel Rand <nathanieljrand@gmail.com>
2024-02-29 23:36:31 -0500

    Finish basic go unit testing, benchmark, and example

---
+++ go_unit_test/go.mod
@@ -0,0 +1,3 @@
+module github.com/NathanielRand/samples/go_unit_test
+
+go 1.21.1
---
+++ go_unit_test/main.go
@@ -0,0 +1,6 @@
+package main
+
+// Note: This file is intentionally left empty.
+// CD into the target directory to run the 
+// provided tests individually or run 'go test'
+// to run all tests in the directory.
